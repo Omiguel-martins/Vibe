6528d35f38268926b6b97ca2d6b8a5cd0289ede6
Omiguel-martins <miguelcarlosmkt@gmail.com>
2026-08-28 00:58:13 -0400

    feat: add productImages root module

---
+++ src/data/productImages.js
@@ -0,0 +1,11 @@
+import { imgBoaMenina } from './images/boaMenina';
+import { imgRosaDaManha } from './images/rosaDaManha';
+import { imgInvencivel } from './images/invencivel';
+import { imgComboTrio } from './images/comboTrio';
+
+export const productImages = {
+  boaMenina: imgBoaMenina,
+  rosaDaManha: imgRosaDaManha,
+  invencivel: imgInvencivel,
+  comboTrio: imgComboTrio
+};
